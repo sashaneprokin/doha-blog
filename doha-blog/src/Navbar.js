7a2dfa8a44b8a7b4b18ca56c67ba51b3db90d588
sneprokin <neprokinalex94@gmail.com>
2021-01-10 20:09:12 +0200

    build2

---
+++ doha-blog/src/Navbar.js
@@ -0,0 +1,13 @@
+const Nav = () => {
+  return ( 
+    <nav className="navbar">
+      <h1>The Doha Blog</h1>
+      <div className="links">
+        <a hreaf="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+   );
+}
+ 
+export default Nav;
