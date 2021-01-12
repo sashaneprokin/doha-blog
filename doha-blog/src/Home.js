0b52ad1a023345c2e3476e26a730b50683e09440
sneprokin <neprokinalex94@gmail.com>
2021-01-12 13:14:42 +0200

    build 4

--- doha-blog/src/Home.js
+++ doha-blog/src/Home.js
@@ -1,20 +1,21 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-  //let name = 'mario';
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(26);
+ const [blogs, setBlogs] = useState([
+  { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+  { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+  { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+ ]);
 
-  const handleClick = () => {
-    setName('luigi');
-    setAge(30);
+ const handleDelete = (id) => {
+   const newBlogs = blogs.filter(blog => blog.id !== id);
+   setBlogs(newBlogs); 
   }
 
   return ( 
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } old</p>
-      <button onClick={handleClick}>Click me</button>
+     <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
     </div>
    );
 }
